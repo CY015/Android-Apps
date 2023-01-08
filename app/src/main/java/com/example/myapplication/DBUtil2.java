35243dcfe9f047d64a76b1071c71e703d6ecdb09
Che Yi <51329293+CY015@users.noreply.github.com>
2023-01-08 22:07:39 +0800

    add CRUD

--- app/src/main/java/com/example/myapplication/DBUtil2.java
+++ app/src/main/java/com/example/myapplication/DBUtil2.java
@@ -1,4 +1,6 @@
 package com.example.myapplication;
+import android.widget.Toast;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -6,6 +8,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBUtil2 {
+
+    private static Connection conn = null;
+
         private static Connection getSQLConnection(String ip, String user, String pwd, String db)
         {
             Connection con = null;
@@ -29,9 +34,12 @@ public class DBUtil2 {
             String result = "";
             try
             {
-                Connection conn = getSQLConnection("192.168.0.104", "sa", "cheyi", "testApp");
-                String sql = "select * from [user]";
-                Statement stmt = conn.createStatement();//
+                if(conn == null){
+                    conn = getSQLConnection("192.168.0.101", "sa", "cheyi", "testApp");
+                }
+
+                String sql = "SELECT * From [user]";
+                Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(sql);
                 while (rs.next())
                 {
@@ -51,8 +59,86 @@ public class DBUtil2 {
             return result;
         }
 
-        public static void main(String[] args)
-        {
-            QuerySQL();
+    /**
+     * 向服务器数据库插入数据
+     */
+    public static int insertIntoData(int uID, String uName) {
+        int result = 0;
+        try {
+            if (conn == null) {
+                conn = getSQLConnection("192.168.0.101", "sa", "cheyi", "testApp");
+            }
+            Statement stmt = conn.createStatement();
+
+            if (conn == null || stmt == null) {
+                return result ;
+            }
+            //插入sql语句（[user] 为表名，userID, name为要插入的字段名）
+            String sql = "INSERT INTO [user](userID, name) VALUES ('"+ uID +"', '"+ uName +"')";
+            result = stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return result ;
+    }
+
+    /**
+     * 数据更新方法
+     */
+    public static int update(int uID, String uName) {
+        int result = 0;
+        try {
+            if (conn == null) {
+                conn = getSQLConnection("192.168.0.101", "sa", "cheyi", "testApp");
+            }
+            Statement stmt = conn.createStatement();
+            if (conn == null || stmt == null) {
+                return result;
+            }
+            // 数据更新sql语句
+            String sqlStr = "update [user] set name = '"+ uName +"' where uID = '"+ uID +"'";
+            // 数据删除sql语句
+            // String sqlStr = "delete from Tb_Id where id = '0001'";
+
+            result = stmt.executeUpdate(sqlStr);
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return result;
+    }
+
+    /**
+     * 数据删除方法
+     */
+    public static int delete(int uID, String uName) {
+        int result = 0;
+        try {
+            if (conn == null) {
+                conn = getSQLConnection("192.168.0.101", "sa", "cheyi", "testApp");
+            }
+            Statement stmt = conn.createStatement();
+            if (conn == null || stmt == null) {
+                return result;
+            }
+            // 数据更新sql语句
+            // String sqlStr = "update Tb_Id set id = '110' where id = '0001'";
+            // 数据删除sql语句
+            String sqlStr = "DELETE FROM [user] WHERE userID = '"+ uID +"'";
+
+            result = stmt.executeUpdate(sqlStr);
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            //Toast.makeText(this, e.getMessage(),Toast.LENGTH_LONG).show();
+
+        }
+        return result;
+    }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,91 +2,211 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btn;
     private TextView tvUName;
+    private Button btn2;
+    private EditText etUID;
+    private EditText etUName;
+    private Button btn3;
+    private EditText etDelUID;
+    private EditText etDelUName;
+    private Button btn4;
+    private EditText etUpdUID;
+    private EditText etUpdUName;
+
+    private Button btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
+        btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+    }
+
+    private void init(){
         tvUName = findViewById(R.id.userName);
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(getClickEvent());
+        btn = findViewById(R.id.button_search);
+
+        etUID = findViewById(R.id.etID);
+        etUName = findViewById(R.id.etName);
+        btn2 = findViewById(R.id.button_insert);
+
+        etDelUID = findViewById(R.id.etDelUID);
+        etDelUName = findViewById(R.id.etDelUName);
+        btn3 = findViewById(R.id.button_delete);
+
+        etUpdUID = findViewById(R.id.etUpdUID);
+        etUpdUName = findViewById(R.id.etUpdUName);
+        btn4 = findViewById(R.id.button_update);
+
+        btn5 = findViewById(R.id.button_test);
     }
 
-//    public void showFirst(View v){
-//        String result = DBUtil.search();
-//        Toast.makeText(this,""+result,Toast.LENGTH_SHORT).show();
-//    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_test:
+                Toast.makeText(MainActivity.this, "test.", Toast.LENGTH_LONG).show();
+                break;
 
-    private View.OnClickListener getClickEvent(){
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvUName.setText("name");
-                if(v==btn){
-                    test();
+            case R.id.button_search:
+                showInfo();
+                break;
+
+            case R.id.button_insert:
+                try{
+                    Integer inUID = Integer.parseInt(etUID.getText().toString());
+                    String inUName = etUName.getText().toString();
+
+                    String inUID_Pattern = "^d{1,}$";
+                    Pattern p = Pattern.compile(inUID_Pattern);
+                    Matcher m = p.matcher(inUID+"");
+
+                    insertInfo(inUID, inUName);
                 }
-            }
-        };
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Please check the id which must be a num.",Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            case R.id.button_delete:
+//                try{
+                    Integer delUID = Integer.parseInt(etDelUID.getText().toString());
+                    String delUName = etDelUName.getText().toString();
+//
+//                    String delUID_Pattern = "^d{1,}$";
+//                    Pattern p = Pattern.compile(delUID_Pattern);
+//                    Matcher m = p.matcher(delUID+"");
+
+                    deleteInfo(delUID, delUName);
+//                }
+//                catch (Exception e){
+//                    Toast.makeText(MainActivity.this, "Please check the id which must be a num.",Toast.LENGTH_LONG).show();
+//                }
+
+            case R.id.button_update:
+                try{
+                    Integer updUID = Integer.parseInt(etUpdUID.getText().toString());
+                    String updUName = etUpdUName.getText().toString();
+
+                    String updUID_Pattern = "^d{1,}$";
+                    Pattern p = Pattern.compile(updUID_Pattern);
+                    Matcher m = p.matcher(updUID+"");
+
+                    updateInfo(updUID, updUName);
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Please check the id which must be a num.",Toast.LENGTH_LONG).show();
+                }
+
+        }
     }
 
-    private void test(){
+    // TODO 只能使用一次对数据库的操作，用过一次之后使用别的功能会失败
+    // 查询结果
+    private void showInfo(){
         Runnable r = new Runnable() {
             @Override
             public void run() {
                 String res = DBUtil2.QuerySQL();
                 tvUName.setText(res);
-
-                // 下面这个好像行不通
-                Message msg = new Message();
-                msg.what = 1001;
-                Bundle data = new Bundle();
-                data.putString("result", res);
-                msg.setData(data);
-                mHandler.sendMessage(msg);
             }
         };
         new Thread(r).start();
     }
 
-    @SuppressLint("HandlerLeak")
-    Handler mHandler = new Handler(){
-        @SuppressLint("HandlerLeak")
-        public void handleMessage(android.os.Message msg){
-            switch (msg.what){
-                case 1001:
-                    String str = msg.getData().getString("result");
-                    tvUName.setText(str);
-                    break;
-
-                default:
-                    break;
+    // 插入数据
+    private void insertInfo(int uid, String uName){
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                Integer i = DBUtil2.insertIntoData(uid, uName);
+                if(i == 0) {
+                    // 不能直接在子线程中弹出Toast，得调用Looper.prepare()
+                    Looper.prepare();
+                    Toast.makeText(MainActivity.this, "Fail to insert. Please check the id/name whether is too long.", Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                }
+                else{
+                    Looper.prepare();
+                    Toast.makeText(MainActivity.this, "Succeed.",Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                }
             }
-        }
-    };
+        };
+        new Thread(r1).start();
+    }
 
+    // 删除数据
+    private void  deleteInfo(int uid, String uName){
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                Integer i = 0;
+                try{
+                    i = DBUtil2.delete(uid, uName);
+                } catch (Exception e){
+                    Looper.prepare();
+                    Toast.makeText(MainActivity.this, "SQL的问题",Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                }
 
-//    public void showFirst(View view) {
-//        Runnable run = new Runnable() {
-//            @Override
-//            public void run() {
-//                String result = DBUtil.search();
-//                DBUtil.closeConnect();
-//                System.out.println(result);
-//            }
-//        };
-//        new Thread(run).start();
-//    }
+                if(i == 0) {
+                    // 不能直接在子线程中弹出Toast，得调用Looper.prepare()
+                    Looper.prepare();
+                    Toast.makeText(MainActivity.this, "Fail to delete. Please check the id/name whether is too long.", Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                }
+                else{
+                    Looper.prepare();
+                    Toast.makeText(MainActivity.this, "Succeed.",Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                }
+            }
+        };
+        new Thread(r2).start();
+    }
+
+    // 更新数据
+    private void  updateInfo(int uid, String uName){
+        Runnable r3 = new Runnable() {
+            @Override
+            public void run() {
+                Integer i = DBUtil2.update(uid, uName);
+                if(i == 0) {
+                    // 不能直接在子线程中弹出Toast，得调用Looper.prepare()
+                    Looper.prepare();
+                    Toast.makeText(MainActivity.this, "Fail to update. Please check the id/name whether is too long.", Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                }
+                else{
+                    Looper.prepare();
+                    Toast.makeText(MainActivity.this, "Succeed.",Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                }
+            }
+        };
+        new Thread(r3).start();
+    }
+
 }
